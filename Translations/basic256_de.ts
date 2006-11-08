4d0bdaf511ee4a2bcc48a0329bf7825e7fe353de
drblast <drblast@deda80ff-9d1e-0410-aade-8f0ad8a54eb3>
2006-11-08 06:24:13 +0000

    git-svn-id: https://svn.code.sf.net/p/kidbasic/code/trunk@105 deda80ff-9d1e-0410-aade-8f0ad8a54eb3

---
+++ Translations/basic256_de.ts
@@ -0,0 +1,266 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>BasicEdit</name>
+    <message>
+        <source>Line: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New Program?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Are you sure you want to completely clear this program and start a new one?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Untitled - BASIC-256</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save file as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BASIC-256 File </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Any File </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> - BASIC-256</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open a file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BASIC-256 file </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Interpreter</name>
+    <message>
+        <source>ERROR on line </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Syntax error on line </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No such label</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Next without FOR</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Array dimension too large</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot assign non-string to string array</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Array index out of bounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot access non-array variable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unknown variable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to int()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to string()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to sin()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to cos()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to tan()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to ceil()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to floor()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Illegal argument to abs()</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>String in numeric expression</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot compare strings with &gt; or &lt;=</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot compare strings with &lt; or &gt;=</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Run</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Pause</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stop</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Step</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&lt;h2 align=&apos;center&apos;&gt;BASIC-256 -- Version 0.8&lt;/h2&gt;                                   &lt;p&gt;Copyright &amp;copy; 2006, Ian Larsen&lt;/p&gt;<byte value="x9"/>                                  &lt;p&gt;&lt;strong&gt;Thanks to our translators:&lt;/strong&gt; Immo-Gert Birn                                   &lt;p&gt;&lt;i&gt;You should have received a copy of the GNU General Public License along&lt;br&gt;                                   with this program; if not, write to the Free Software Foundation, Inc.,&lt;br&gt;                                   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.&lt;/i&gt;&lt;/p&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save As</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Copy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paste</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Debug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save Compiled Byte Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About BASIC-256</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Graphics Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Text Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ready.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Untitled - BASIC-256</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RunController</name>
+    <message>
+        <source>Running</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Ready.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Paused</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save file as</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BASIC-256 Compiled File </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Any File </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
