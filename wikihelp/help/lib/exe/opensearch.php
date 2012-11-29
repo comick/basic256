1b9175c8a93f568c06aefb39217c4dd104c530fc
renejm <renejm@deda80ff-9d1e-0410-aade-8f0ad8a54eb3>
2012-11-29 22:22:01 +0000

    git-svn-id: https://svn.code.sf.net/p/kidbasic/code/trunk@567 deda80ff-9d1e-0410-aade-8f0ad8a54eb3

---
+++ wikihelp/help/lib/exe/opensearch.php
@@ -0,0 +1,7 @@
+<?xml version="1.0"?>
+<OpenSearchDescription xmlns="http://a9.com/-/spec/opensearch/1.1/">
+  <ShortName>BASIC 256 - Language Documentation</ShortName>
+  <Image width="16" height="16" type="image/x-icon">http://doc.basic256.org/lib/tpl/dokuwiki/images/favicon.ico</Image>
+  <Url type="text/html" template="http://doc.basic256.org/doku.php?do=search&amp;id={searchTerms}" />
+  <Url type="application/x-suggestions+json" template="http://doc.basic256.org/lib/exe/ajax.php?call=suggestions&amp;q={searchTerms}" />
+</OpenSearchDescription>
